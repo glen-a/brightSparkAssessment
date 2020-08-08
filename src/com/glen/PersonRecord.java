831609a132de40e47f01c1c55c2d49b3e0e624d9
glen wiltshire <glen@wilt.sh>
2020-08-08 17:52:08 +1000

    Read from CSV (hardcoded path). Add getters/setters and a temporary toString for the PersonRecord class for debugging.

--- src/com/glen/PersonRecord.java
+++ src/com/glen/PersonRecord.java
@@ -1,16 +1,90 @@
 package com.glen;
 
 public class PersonRecord {
-    	//date: String (format YYYY-MM-DD)
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName.trim();
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName.trim();
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date.trim();
+    }
+
+    public Integer getDivision() {
+        return division;
+    }
+
+    public void setDivision(Integer division) {
+        this.division = division;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary.trim();
+    }
 
     String firstName;
     String lastName;
+    // date: String (format YYYY-MM-DD)
     String date;
-    Integer devision;
+    Integer division;
     Integer points;
     String summary;
 
+    public PersonRecord(String[] attributes) {
+        if (!isNumber(attributes[2])) {
+            //throw division unexpected format
+        }
+        if (!isNumber(attributes[3])) {
+            //throw points unexpected format
+        }
 
+        this.firstName = attributes[0];
+        this.lastName = attributes[1];
+        this.date = attributes[2];
+        this.division = Integer.parseInt(attributes[3]);
+        this.points = Integer.parseInt(attributes[4]);
+        this.summary = attributes[5];
 
+    }
+
+    public String toString() {
+        return "name : " + getFirstName() + " " + getLastName() +
+                ", date: " + getDate()+
+                ", division: " + getDivision()+
+                ", points: " + getPoints()+
+                ", summary: " + getSummary() ;
+    }
+
+    //should exist in some util class
+    private Boolean isNumber(String str) {
+        return (str.length() > 1 && str.matches("^[0-9]*$"));
+    }
 }
