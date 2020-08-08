f081001980fb1072c89633fba4d6dfcd7b66fee0
glen wiltshire <glen@wilt.sh>
2020-08-08 16:26:03 +1000

    initial commit

---
+++ src/com/glen/Main.java
@@ -0,0 +1,41 @@
+package com.glen;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+        System.out.println("Import from file or clipboard?");
+        //get user input f or c
+
+
+        //file
+        //get path
+        String path = "";
+
+        readRecordsFromFile(path);
+        //clipboard
+
+
+
+
+
+
+        PersonRecord pr = new PersonRecord();
+
+
+
+
+
+    }
+
+
+    private static List<PersonRecord> readRecordsFromFile(String path){
+        List<PersonRecord> records = new ArrayList<>();
+
+        return records;
+    }
+}
---
+++ src/com/glen/PersonRecord.java
@@ -0,0 +1,16 @@
+package com.glen;
+
+public class PersonRecord {
+    	//date: String (format YYYY-MM-DD)
+
+
+    String firstName;
+    String lastName;
+    String date;
+    Integer devision;
+    Integer points;
+    String summary;
+
+
+
+}
